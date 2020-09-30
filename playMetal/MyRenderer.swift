1f09b3a6a35f9e79cabb218120927eb510220d8b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-30 05:28:19 -0700

    [0]

--- playMetal/MyRenderer.swift
+++ playMetal/MyRenderer.swift
@@ -25,7 +25,7 @@ class MyRenderer: NSObject {
     }
     
     static private func createVertexBuffer(device:MTLDevice) ->MTLBuffer {
-        let vertices = Array(0...360).map { (i) -> [simd_float2] in
+        var vertices = Array(0..<360).map { (i) -> [simd_float2] in
             let rad0 = Float(i * 2) / 2.0 / 180 * .pi
             let rad1 = Float(i * 2 + 1) / 2.0 / 180 * .pi
             return [
@@ -34,6 +34,7 @@ class MyRenderer: NSObject {
                 [0, 0]
             ]
         }.flatMap { $0 }
+        vertices.append(vertices[0])
         
         return device.makeBuffer(bytes: vertices, length: vertices.count * MemoryLayout<simd_float2>.stride, options: [])!
     }
