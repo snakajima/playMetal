4583c666ab8c485eb736586eb2eba177b3321d82
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-29 17:16:12 -0700

    Initial Commit

---
+++ playMetal/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  playMetal
+//
+//  Created by SATOSHI NAKAJIMA on 9/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
