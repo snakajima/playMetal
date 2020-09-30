7ebefdbd0fd07dc4a4d8ead32c55075c712a98a9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-29 17:23:25 -0700

    MyMetalView

---
+++ playMetal/MyMetalView.swift
@@ -0,0 +1,33 @@
+//
+//  MyMetalView.swift
+//  playMetal
+//
+//  Created by SATOSHI NAKAJIMA on 9/29/20.
+//
+
+import SwiftUI
+
+struct MyMetalView: NSViewRepresentable {
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    public func makeNSView(context: Context) -> some NSView {
+        let nsView = NSView()
+        let layer = CALayer()
+        layer.backgroundColor = NSColor.yellow.cgColor
+        nsView.layer = layer
+        return nsView
+    }
+    
+    public func updateNSView(_ nsView: NSViewType, context: Context) {
+        //
+    }
+    
+    class Coordinator: NSObject {
+        let view: MyMetalView
+        init(_ view:MyMetalView) {
+            self.view = view
+        }
+    }
+}
