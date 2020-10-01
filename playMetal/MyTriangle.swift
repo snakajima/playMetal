d397bca73f0b564e476a8560a2c5163d10f285e2
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-30 19:57:05 -0700

    rotation

--- playMetal/MyTriangle.swift
+++ playMetal/MyTriangle.swift
@@ -18,11 +18,11 @@ class MyTriangle: MyRendererDelegate {
         let time = Float(CACurrentMediaTime() * .pi)
         var vertices:[simd_float2] = [
             simd_float2(0,0),
-            simd_float2(sinf(time),0),
-            simd_float2(0,cosf(time)),
-            simd_float2(-sinf(time),0),
-            simd_float2(0,-cosf(time)),
-            simd_float2(sinf(time),0)
+            simd_float2(sinf(time),cosf(time)),
+            simd_float2(-cosf(time),sinf(time)),
+            simd_float2(-sinf(time),-cosf(time)),
+            simd_float2(cosf(time),-sinf(time)),
+            simd_float2(sinf(time),cosf(time))
         ]
         vertices.append(vertices[0])
         return vertices
